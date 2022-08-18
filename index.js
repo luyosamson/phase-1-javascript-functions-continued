2820d4d936ad520e0ae2f8a5720c51acc741c5d3
luyosamson <luyosamson@gmail.com>
2022-08-18 21:00:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(myTask ='roller-skate'){
+
+    return (`This Saturday, I want to ${myTask}!`);
+
+}
+saturdayFun('bathe my dog');
+
+//Lab 2: Implement a function expression called mondayWork. The function should
+// return a String like "This Monday, I will ... ." Fill in the ... with the activity that's passed in as 
+//the first parameter. If nothing is passed in, default to "go to the office"
+
+const mondayWork=function(activity ='go to the office'){
+
+return `This Monday, I will ${activity}.`
+}
+mondayWork('attend a meeting')
+
+
+function wrapAdjective(style = "||") {
+  return function (innerParam = "special") {
+    return `You are ${style}${innerParam}${style}!`;
+  }}
